3993579a8b61d661db9fc1c1588e186c5b6af3b9
Biljana  Nedic <biljana.nedic@brezasoftware.com>
2022-08-16 15:07:02 +0200

    obrisani importi koji se ne koriste

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,7 @@
 import React from 'react'
-import { useHistory, Link } from "react-router-dom";
+import { useHistory} from "react-router-dom";
 import { useState } from 'react';
-import { login } from '../services/auth';
+//import { login } from '../services/auth';
 
 
 
